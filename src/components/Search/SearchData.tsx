654a8aa72e529f3d583e5e2d2c0eba200abab487
Oscar Montes Camberos <o.montescamberos@gmail.com>
2024-11-30 14:02:34 -0600

    Fix attempt Vercel deploy #2

--- src/components/Search/SearchData.tsx
+++ src/components/Search/SearchData.tsx
@@ -50,7 +50,7 @@ export default function SearchData() {
 
     setTypingTimeout(setTimeout(() => {
       handleAPIRequest(newText);
-    }, MAX_SEARCH_DELAY) as number);
+    }, MAX_SEARCH_DELAY) as unknown as number);
   }
 
   useEffect(() => {
